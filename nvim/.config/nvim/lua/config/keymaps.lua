0a6c5e054d1e44ff3af958235a1ed0b1a22b02fb
Jack McGill <mr.mcgilljh@gmail.com>
2026-08-22 21:03:57 +1000

    add lsp formatting and organise import keybinds

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -22,19 +22,19 @@ map("n", "<C-d>", "<C-d>zz", { desc = "Half page down" })
 -- Open netrw
 map("n", "<leader>e", vim.cmd.Ex, { desc = "Open file explorer" })
 
--- Quick switch to last edited file 
+-- Quick switch to last edited file
 map("n", "<leader>bb", "<cmd>e #<cr>", { desc = "Switch to Other Buffer" })
 
--- SNACKS
+-- ════ SNACKS
 map("n", "<leader>ff", function() Snacks.picker.files() end, { desc = "Find Files" })
 map("n", "<leader>fg", function() Snacks.picker.grep() end, { desc = "Grep" })
 
 
 -- ═══════════════════════════════════════════════════════════
--- WINDOW MANAGEMENT 
+-- WINDOW MANAGEMENT
 -- ═══════════════════════════════════════════════════════════
 
--- Move between windows with Ctrl+hjkl 
+-- Move between windows with Ctrl+hjkl
 map("n", "<C-h>", "<C-w>h", { desc = "Go to Left Window", remap = true })
 map("n", "<C-j>", "<C-w>j", { desc = "Go to Lower Window", remap = true })
 map("n", "<C-k>", "<C-w>k", { desc = "Go to Upper Window", remap = true })
@@ -45,7 +45,7 @@ map("n", "<C-l>", "<C-w>l", { desc = "Go to Right Window", remap = true })
 -- TEXT EDITING
 -- ═══════════════════════════════════════════════════════════
 
--- Auto-close pairs 
+-- Auto-close pairs
 map("i", "`", "``<left>")
 map("i", '"', '""<left>')
 map("i", "(", "()<left>")
@@ -57,3 +57,20 @@ map("i", "<", "<><left>")
 map("i", ",", ",<c-g>u")
 map("i", ".", ".<c-g>u")
 map("i", ";", ";<c-g>u")
+
+
+-- ═══════════════════════════════════════════════════════════
+-- TEXT FORMATTING
+-- ═══════════════════════════════════════════════════════════
+
+-- Formatting
+map("n", "<leader>cf", function() vim.lsp.buf.format() end, { desc = "Format Document" })
+
+-- Imports
+vim.keymap.set("n", "<leader>oi", function()
+  vim.lsp.buf.code_action({
+    context = {
+      only = { "source.organizeImports" },
+    },
+  })
+end, { desc = "Organize imports" })
